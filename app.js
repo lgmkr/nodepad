e23416f447c81d7c059c6b5c920b5e812b342c0c
Oleg Makarov <gstancox@gmail.com>
2012-07-07 19:54:06 +0300

    add creat/update for documents

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var express = require('express')
   db = mongoose.connect('mongodb://localhost/nodepad'),
   Document = require('./models.js').Document(db);
 
+var ObjectId = mongoose.Types.ObjectId;
+
 var app = module.exports = express.createServer();
 
 // Configuration
@@ -59,6 +61,44 @@ app.get('/documents.:format?', function(req, res) {
     };
   });
 });
+
+app.get('/documents/:id.:format?/edit', function(req, res) {
+  Document.findOne({'_id': new ObjectId(req.params.id)}, function(err, d) {
+    res.render('documents/edit.jade', { d: d });
+  });
+});
+
+app.get('/documents/new', function(req, res) {
+  res.render('documents/new.jade', { d: new Document() });
+});
+
+app.post('/documents', function(req, res){
+  var d = new Document;
+  d.title = req.body.document.title;
+  d.tags = req.body.document.tags;
+  d.save(function(){
+    res.redirect('/documents');
+  })
+
+});
+
+app.post('/documents/:id.:format?', function(req, res) {
+  Document.findOne({'_id': new ObjectId(req.params.id)}, function(err, d) {
+    d.title = req.body.document.title;
+    d.data = req.body.document.data;
+    d.save(function() {
+      // Выдаем результат в запрошенном формате
+      switch (req.params.format) {
+        case 'json':
+          res.send(d.__doc);
+         break;
+
+         default:
+          res.redirect('/documents');
+      }
+    });
+  });
+})
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- models.js
+++ models.js
@@ -3,9 +3,14 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema
   , objectId = schema.ObjectId;
 
+function idToHex() {
+  return this._id.toHexString();
+}
+
 var documents = new schema({
-  title  :String,
-  tags   :String
+  id    : {type: objectId, get: idToHex}, 
+  title  : {type: String},
+  tags   : {type: String}
 });
 //mongoose.model('Document', {
 //    properties: ['title', 'data', 'tags'],
